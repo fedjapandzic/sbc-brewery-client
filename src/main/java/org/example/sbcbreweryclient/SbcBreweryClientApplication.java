bc63eff2a9f6729e11f034d45390f7ed20d672bf
Fedja Pandzic <fedjapandzic1@gmail.com>
2024-10-30 14:53:47 +0100

    Initial commit

---
+++ src/main/java/org/example/sbcbreweryclient/SbcBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package org.example.sbcbreweryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbcBreweryClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbcBreweryClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sbcbreweryclient/SbcBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sbcbreweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbcBreweryClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
